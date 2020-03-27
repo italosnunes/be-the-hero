fda7e3755aae0fe41875e4ce393db3833e278e24
italosnunes <46608041+italosnunes@users.noreply.github.com>
2020-03-27 12:55:37 -0300

    incluido testes de exemplos

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 const app = express();
-const port = 3333;
+
 
 app.use(cors({}));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
+
+app.use(errors());
 
 
 
-app.listen(port)
-console.log('Backend rodando na porta ' + port );
+module.exports = app;
